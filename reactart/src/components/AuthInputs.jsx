726a63b61d75bd46fb1ac6b6195459e9d9a3bc4e
NirajMourya <nirajmourya786@gmail.com>
2023-12-29 17:30:32 +0530

    added dynamic conditional classes

--- reactart/src/components/AuthInputs.jsx
+++ reactart/src/components/AuthInputs.jsx
@@ -25,24 +25,24 @@ export default function AuthInputs()
     <div id="auth-inputs">
     <div className="controls">
       <p>
-        <label>Email</label>
+        <label className={`label ${emailNotValid ? 'invalid':''}`}>Email</label>
         <input
           type="email"
-          style={{
-            background:emailNotValid ? '#fed2d2': '#d1d5db'
-          }}
-          //className={emailNotValid ? 'invalid' : undefined}
+        //   style={{
+        //     background:emailNotValid ? '#fed2d2': '#d1d5db'
+        //   }}
+          className={emailNotValid ? 'invalid' : undefined}
           onChange={(event) => handleInputChange('email', event.target.value)}
         />
       </p>
       <p>
-        <label>Password</label>
+        <label className={`label ${passwordNotValid ? 'invalid':''}`}>Password</label>
         <input
           type="password"
-          style={{
-            background:emailNotValid ? '#fed2d2': '#d1d5db'
-          }}
-          //className={passwordNotValid ? 'invalid' : undefined}
+        //   style={{
+        //     background:passwordNotValid ? '#fed2d2': '#d1d5db'
+        //   }}
+          className={passwordNotValid ? 'invalid' : undefined}
           onChange={(event) =>
             handleInputChange('password', event.target.value)
           }
